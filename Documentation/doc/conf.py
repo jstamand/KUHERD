07030025cc2d5848b7b0626b8324d6bc9bd4a89a
Joseph St.Amand <josephstamand@gmail.com>
2017-05-09 16:44:51 -0400

    Added licensing to all files

--- Documentation/doc/conf.py
+++ Documentation/doc/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'KUHERD'
 copyright = '2017, Joseph St.Amand'
-author = 'Joseph St.Amand'
+author = ''
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- LabelSets.py
+++ LabelSets.py
@@ -1,3 +1,18 @@
+#    Copyright (C) 2017  Joseph St.Amand
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 purpose = {'applied': 1, 'basic': 2, 'development': 3, 'other': 4, 'researchtr': 5, 'service': 6, 'training': 7}
 field = {'a1': 1, 'a2': 2, 'a3': 3, 'a4': 4, 'a5': 5, 'a6': 6, 'a7': 7, 'a8': 8, 'a9': 9, 'b1': 10, 'b2': 11, 'b3': 12,
          'b4': 13, 'b5': 14, 'c1': 15, 'c2': 16, 'c3': 17, 'c4': 18, 'chembio': 19, 'd': 20, 'e': 21, 'f1': 22,
